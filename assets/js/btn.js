67f22cb99a388b93dfa624b14add61c97cd24c53
anndreww <antoniogradisteanu@gmail.com>
2022-05-11 18:25:37 +0300

    commit on 'masterbranch'

---
+++ assets/js/btn.js
@@ -0,0 +1,48 @@
+function problemsCtrl($scope) {
+
+  $scope.problems = [
+    {
+      name:"Watermelon",
+      id:"#1",
+      source:"CODEFORCES",
+      link:"https://codeforces.com/problemset/problem/4/A",
+    },{
+      name:"Miami GP",
+      id:"#2",
+      source:"CODECHEF",
+      link:"https://www.codechef.com/problems/F1RULE",
+    },{
+      name:"Boy or Girl",
+      id:"#3",
+      source:"CODECHEF",
+      link:"https://codeforces.com/problemset/problem/236/A",
+    },{
+      name:"Football",
+      id:"#4",
+      source:"CODEFORCES",
+      link:"https://codeforces.com/problemset/problem/96/A",
+    },{
+      name:"Chefland Visa",
+      id:"#5",
+      source:"CODECHEF",
+      link:"https://www.codechef.com/problems/VISA",
+    },{
+      name:"Media Geometrica",
+      id:"#6",
+      source:"PBINFO",
+      link:"https://www.pbinfo.ro/probleme/2299/media-geometrica",
+    },{
+      name:"CMMMDC",
+      id:"#7",
+      source:"INFOARENA",
+      link:"https://www.infoarena.ro/problema/cmmdc",
+    },{
+      name:"Factorial",
+      id:"#8",
+      source:"INFOARENA",
+      link:"https://www.infoarena.ro/problema/fact",
+    }
+    
+  ]
+
+}
